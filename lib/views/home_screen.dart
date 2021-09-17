6ac04a61331609d4999de06426fefb827d998832
dungngminh <ngminhdung1311@gmail.com>
2021-09-18 02:13:55 +0700

    fixing overflow

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -61,185 +61,189 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
-              width: double.infinity,
-              child: Column(
-                children: [
-                  BlocBuilder<WeatherBloc, WeatherState>(
-                    builder: (context, state) {
-                      return Text(
-                        state.main?.toUpperCase() ?? 'Loading...',
-                        style: GoogleFonts.montserrat(
-                          color: Colors.black,
-                          fontSize: 40,
-                        ),
-                      );
-                    },
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                    ),
-                    height: 200,
-                    width: 200,
-                    child: BlocBuilder<WeatherBloc, WeatherState>(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 16, horizontal: 10),
+                height: size.height,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    BlocBuilder<WeatherBloc, WeatherState>(
                       builder: (context, state) {
-                        if (state.weatherStatus is WeatherIsLoading)
-                          return CircularProgressIndicator();
-                        else if (state.weatherStatus is WeatherLoaded)
-                          return Image.asset(
-                            "assets/icon_weather/${state.icon}.png",
-                          );
-                        else
-                          return Icon(Icons.error);
+                        return Text(
+                          state.main?.toUpperCase() ?? 'Loading...',
+                          style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            fontSize: 40,
+                          ),
+                        );
                       },
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-                    width: double.infinity,
-                    child: IntrinsicHeight(
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 15.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                BlocBuilder<WeatherBloc, WeatherState>(
-                                  builder: (context, state) {
-                                    return Text(
-                                      "${state.temp?.round() ?? 0}",
-                                      style: TextStyle(
-                                        fontSize: 70,
-                                        fontWeight: FontWeight.w400,
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      height: 200,
+                      width: 200,
+                      child: BlocBuilder<WeatherBloc, WeatherState>(
+                        builder: (context, state) {
+                          if (state.weatherStatus is WeatherIsLoading)
+                            return CircularProgressIndicator();
+                          else if (state.weatherStatus is WeatherLoaded)
+                            return Image.asset(
+                              "assets/icon_weather/${state.icon}.png",
+                            );
+                          else
+                            return Icon(Icons.error);
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                      width: double.infinity,
+                      child: IntrinsicHeight(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  BlocBuilder<WeatherBloc, WeatherState>(
+                                    builder: (context, state) {
+                                      return Text(
+                                        "${state.temp?.round() ?? 0}",
+                                        style: TextStyle(
+                                          fontSize: 70,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                        textAlign: TextAlign.start,
+                                      );
+                                    },
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "0",
+                                    style: GoogleFonts.montserrat(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // SizedBox(
+                            //   width: 15,
+                            // ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Details".toUpperCase(),
+                                    style: GoogleFonts.montserrat(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w500,
+                                      letterSpacing: 3,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 2,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          _detailLabel('Feels like'),
+                                          _detailLabel('humidity'),
+                                          _detailLabel('wind'),
+                                          _detailLabel('visibility'),
+                                        ],
                                       ),
-                                      textAlign: TextAlign.start,
-                                    );
-                                  },
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  "0",
-                                  style: GoogleFonts.montserrat(
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w500,
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      BlocBuilder<WeatherBloc, WeatherState>(
+                                        builder: (context, state) {
+                                          return Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              _detailData(
+                                                  '${state.feelsLike?.round() ?? 0}°'),
+                                              _detailData(
+                                                  '${state.humidity?.round() ?? 0}%'),
+                                              _detailData(
+                                                  '${state.wind ?? 0} Km/h'),
+                                              _detailData(
+                                                  '${state.visibility != null ? state.visibility! / 1000 : 00} km'),
+                                            ],
+                                          );
+                                        },
+                                      ),
+                                    ],
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                          // SizedBox(
-                          //   width: 15,
-                          // ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Details".toUpperCase(),
-                                  style: GoogleFonts.montserrat(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w500,
-                                    letterSpacing: 3,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 2,
-                                ),
-                                Row(
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        _detailLabel('Feels like'),
-                                        _detailLabel('humidity'),
-                                        _detailLabel('wind'),
-                                        _detailLabel('visibility'),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    BlocBuilder<WeatherBloc, WeatherState>(
-                                      builder: (context, state) {
-                                        return Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            _detailData(
-                                                '${state.feelsLike?.round() ?? 0}°'),
-                                            _detailData(
-                                                '${state.humidity?.round() ?? 0}%'),
-                                            _detailData(
-                                                '${state.wind ?? 0} Km/h'),
-                                            _detailData(
-                                                '${state.visibility != null ? state.visibility! / 1000 : 00} km'),
-                                          ],
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Divider(
-                    indent: 20,
-                    endIndent: 20,
-                    thickness: 2,
-                    color: Colors.grey,
-                  ),
-                  Container(
-                    height: size.height * 0.1,
-                    width: double.infinity,
-                    child: Center(
-                      child: Text(
-                        dayOfWeek,
-                        style: GoogleFonts.montserrat(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 40,
+                          ],
                         ),
                       ),
                     ),
-                  ),
-                  Divider(
-                    indent: 20,
-                    endIndent: 20,
-                    thickness: 2,
-                    color: Colors.grey,
-                  ),
-                  Container(
-                    height: size.height * 0.1,
-                    width: double.infinity,
-                    child: Center(
-                      child: Text(
-                        partOfDay(),
-                        style: GoogleFonts.montserrat(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 40,
+                    Divider(
+                      indent: 20,
+                      endIndent: 20,
+                      thickness: 2,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: size.height * 0.1,
+                      width: double.infinity,
+                      child: Center(
+                        child: Text(
+                          dayOfWeek,
+                          style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 40,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    Divider(
+                      indent: 20,
+                      endIndent: 20,
+                      thickness: 2,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: size.height * 0.1,
+                      width: double.infinity,
+                      child: Center(
+                        child: Text(
+                          partOfDay(),
+                          style: GoogleFonts.montserrat(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 40,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
